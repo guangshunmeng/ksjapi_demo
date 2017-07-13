3964d64b3d2fbc2aad25e819ab1072524c5dbc05
sl <sl@catchbest.com>
2017-07-13 15:53:28 +0800

    update ksjapi

--- KSJApi.Inc/KSJApiBase.h
+++ KSJApi.Inc/KSJApiBase.h
@@ -70,7 +70,7 @@ extern "C"{
 		KSJ_MUC130C_MRYY,    // Not Support
 		KSJ_MUC130M_MRYY,    // Not Support
 		KSJ_MUC320C_MRYY,    // Not Support
-		KSJ_MUC500C_MRYY,    // Not Support
+		KSJ_MUC500C_MRYYO,   // Jelly2
 		KSJ_MUC500M_MRYYO,   // Jelly2
 		KSJ_MUC900C_MRYY,    // Not Support
 		KSJ_MUC1000C_MRYY,   // Not Support
@@ -129,8 +129,10 @@ extern "C"{
 		KSJ_MU3S1200M_SRYYO,
 		KSJ_MU3L2K7C_AGYYO,
 		KSJ_MU3L2K7M_AGYYO,
-		KSJ_MU3L4K3C_AGYYO,
-		KSJ_MU3L4K3M_AGYYO
+		KSJ_MU3L4K3C_AGYYO,    // 100
+		KSJ_MU3L4K3M_AGYYO,
+		KSJ_MU3HS2000C_SRYYO,
+		KSJ_MU3HS2000M_SRYYO
 	};
 	// Enable or Disable Log output
 	KSJ_API  int __stdcall KSJ_LogSet(bool bEnable, const TCHAR *pszFolder);
